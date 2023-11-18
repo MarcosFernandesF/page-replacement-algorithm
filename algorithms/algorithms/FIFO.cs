0b18724c4659a2a72c84e80e386ac50eba44f19e
MarcosFernandesF <marcosrff15@gmail.com>
2023-11-18 15:25:33 -0300

    Criando algoritmo FIFO

---
+++ algorithms/algorithms/FIFO.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+
+namespace algorithms
+{
+    public class FIFO
+	{
+		private int NumeroDeQuadros { get; set; }
+		private List<int> PaginasReferenciadas { get; set; }
+		private Queue<int> OrdemDeAlocacao { get; set; }
+		private int PageFaults { get; set; }
+
+		public FIFO(List<int> paginaReferenciadas, int numeroDeQuadros)
+		{
+			NumeroDeQuadros = numeroDeQuadros;
+			PaginasReferenciadas = paginaReferenciadas;
+            OrdemDeAlocacao = new Queue<int>(numeroDeQuadros);
+			PageFaults = 0;
+		}
+
+		public int ObterPageFaults()
+		{
+            foreach (int pagina in PaginasReferenciadas)
+			{
+				if (!PaginaEstaAlocada(pagina))
+				{
+					if (!FilaEstaCheia())
+					{
+						OrdemDeAlocacao.Enqueue(pagina);
+					}
+					else 
+					{
+						OrdemDeAlocacao.Dequeue();
+                        OrdemDeAlocacao.Enqueue(pagina);
+                    }
+
+					PageFaults++;
+				}
+			}
+
+			return PageFaults;
+		}
+
+        private bool PaginaEstaAlocada(int pagina)
+        {
+			return OrdemDeAlocacao.Contains(pagina);
+        }
+
+		private bool FilaEstaCheia()
+		{
+			return OrdemDeAlocacao.Count == NumeroDeQuadros;
+		}
+    }
+}
---
+++ algorithms/algorithms/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace algorithms
+{
+    public class Program
+	{
+		static void Main(string[] args)
+		{
+			int numeroDeQuadros = 5;
+			List<int> paginasReferenciadas = new List<int> { 1, 3, 10, 3, 20, 245, 12, 1, 4, 5, 4, 7, 210 };
+
+            FIFO algoritmoFifo = new FIFO(paginasReferenciadas, numeroDeQuadros);
+
+			int fifoPageFaults = algoritmoFifo.ObterPageFaults();
+
+            Console.WriteLine($"FIFO Page Faults: {fifoPageFaults}");
+        }
+	}
+}
